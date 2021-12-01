73a00ba7d9f2818aefb8a869da422e53cf1a3c5d
Samara Martins Ferreira <90880046+SamaraMF19@users.noreply.github.com>
2021-12-01 18:53:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+xNome = input("Digite o nome do Aluno: ")
+i = 1
+xSoma = 0
+while (i <= 3):
+  xNota = float(input("Digite a nota {0} do(a) aluno(a): ".format(i)))
+  xSoma = xSoma + xNota
+  i = i + 1
+pass 
+xMedia = xSoma/3
+if (xMedia >= 10):
+  print("Média das notas do aluno {0} é {1}. Aluno(a) aprovado(a)!".format(xNome, xMedia))
+else: 
+  print("Média das notas do aluno {0} é {1}. Aluno(a) reprovado(a)!".format(xNome, xMedia))
+
+
